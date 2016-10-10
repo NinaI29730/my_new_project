b171e66baf8700286d136a500e960839d9c07cd5
Nina Wilson <nina.wilson.mlis@gmail.com>
2016-10-10 15:44:32 -0400

    people

---
+++ db/migrate/20161010193413_create_people.rb
@@ -0,0 +1,12 @@
+class CreatePeople < ActiveRecord::Migration
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.string :age
+      t.string :city
+      t.string :twitter
+
+      t.timestamps null: false
+    end
+  end
+end
